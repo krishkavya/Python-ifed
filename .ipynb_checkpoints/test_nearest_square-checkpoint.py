436b772cb2bc225eb78be090cf9580f78c1fca33
krishkavya <krishkavya03@gmail.com>
2021-01-10 01:31:29 +0530

    Added

--- .ipynb_checkpoints/test_nearest_square-checkpoint.py
+++ .ipynb_checkpoints/test_nearest_square-checkpoint.py
@@ -1,10 +1,10 @@
 from nearest_square import nearest_square
 
-def test_nearest_square_5():
+def test_nearest_square():
     assert(nearest_square(5)==4)
-def test_nearest_square_n12():
+def test_nearest_square():
     assert(nearest_square(-12)==0)
-def test_nearest_square_9():
+def test_nearest_square():
     assert(nearest_square(9)==9)
-def test_nearest_square_23():
+def test_nearest_square():
     assert(nearest_square(23)==16)
